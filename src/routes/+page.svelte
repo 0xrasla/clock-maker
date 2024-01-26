a5fae769021a2e9dbe76993456db8c785a8c8463
rasla <programmer.rasla@gmail.com>
2024-01-26 14:10:35 +0530

    updated love message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import timezones from '../timezone.json';
+	import { fade } from 'svelte/transition';
 
 	let time = '';
 	let selectedTimeZone = 'Asia/Kolkata'; // Default to New York time
 	let convertedTime = '';
+	let showSite = false;
+
+	onMount(() => {
+		setTimeout(() => {
+			showSite = true;
+		}, 4000);
+	});
 
 	function getTimezoneOptions() {
 		return timezones.map((timezone) => {
@@ -52,9 +61,22 @@
 </script>
 
 <main class="section has-text-centered">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
+
+	{#if !showSite}
+		<div transition:fade|local class="fullscreen-background">
+			<section class="section love-message">
+				<div class="container has-text-centered">
+					<i class="fas fa-heart heart-icon"></i>
+					<p class="quote">"You are my heart, my life, my one and only thought Sorna 😘"</p>
+					<p class="author">- Rasla</p>
+				</div>
+			</section>
+		</div>
+	{/if}
+
 	<h1 class="title">Clock Maker (v1)</h1>
 	<p class="subtitle">Convert Indian time to any timezone you want</p>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
 
 	<section class="container is-desktop is-relative is-fullheight">
 		<div class="field">
@@ -104,3 +126,37 @@
 		</p>
 	</section>
 </main>
+
+<style>
+	.fullscreen-background {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		color: black;
+		background: white; /* Add a semi-transparent black background */
+		z-index: 1000; /* Set a high z-index value */
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.love-message {
+		padding: 20px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.heart-icon {
+		font-size: 3em;
+		color: red;
+		margin-bottom: 20px;
+	}
+
+	.quote {
+		font-size: 1.2em;
+		margin-bottom: 20px;
+		text-align: center;
+	}
+</style>
